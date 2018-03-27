3bff550219643530d2a6085779d2934567f695b3
megha54 <37540971+megha54@users.noreply.github.com>
2018-03-27 15:18:14 +0530

    Add files via upload

---
+++ media.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+
+class Movie():
+    # a class that represents a movie
+    __name__ = "Movie House"
+    # Attributes are:movie_title,movie_storyline,poster_image, trailer_youtube
+
+    def __init__(
+            self, movie_title, movie_storyline,
+            poster_image, trailer_youtube):
+                self.title = movie_title
+                self.storyline = movie_storyline
+                self.poster_image_url = poster_image
+                self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        # it opens a trailer in the web browser
+        webbrowser.open(self.trailer_youtube_url)
